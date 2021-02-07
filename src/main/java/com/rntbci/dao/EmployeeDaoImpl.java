1ef49b6a7d1a65c167f85b9454e183f4c57e9c43
bhargav komali <bhargavareddy.komali@gmail.com>
2021-02-07 14:20:51 +0530

    2nd commit

--- src/main/java/com/rntbci/dao/EmployeeDaoImpl.java
+++ src/main/java/com/rntbci/dao/EmployeeDaoImpl.java
@@ -5,47 +5,65 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bson.Document;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.rntbci.db.DbConn;
 import com.rntbci.model.Employee;
 
 public class EmployeeDaoImpl implements IEmployeeDao {
-
+	MongoCollection<Document> collection=DbConn.getConn();
+	ArrayList<Employee> list=null;
 	public Integer saveEmployee(Employee e) {
-		// TODO Auto-generated method stub
-		String sql=" INSERT INTO EMPLOYEES VALUES(?,?,?,?,?) ";
+
+
 		int count=0;
 		try {
-			PreparedStatement pstmt=DbConn.getConn().prepareStatement(sql);
+			list=new ArrayList<Employee>();
 
-			pstmt.setInt(1, e.getEid());
-			pstmt.setString(2, e.getEname());
-			pstmt.setString(3, e.getDesignation());
-		
+			Employee e1=new Employee();
+			e1.setEid(1);
+			e1.setEname("Raghu");
+			e1.setDesignation("worlker");
+			list.add(e1);
 
-			count=pstmt.executeUpdate();
+			Employee e2=new Employee();
+			e2.setEid(2);
+			e2.setEname("Raghu1");
+			e1.setDesignation("owner");
+			list.add(e2);
 
+
+			Employee e3=new Employee();
+			e3.setEid(3);
+			e3.setEname("Raghu3");
+			e3.setDesignation("worlker2");
+			list.add(e3);
+
+		count=	collection.insertOne(list);
 		} catch (Exception ex) {
 			ex.printStackTrace();
 
-	}
+		}
 		return count;
 	}
 
 	public List<Employee> getAllEmployees() {
-		String sql=" SELECT * FROM EMPLOYEES ";
+		MongoCursor<Document> cur=collection.find().iterator();
 		List<Employee> list=null;
 		try {
-			PreparedStatement pstmt=DbConn.getConn().prepareStatement(sql);
-			ResultSet rs=pstmt.executeQuery();
+		
 			list=new ArrayList<>();
-			while(rs.next()) {
-				list.add(new Employee(
-						rs.getInt("eid"), 
-						rs.getString("ename"), 
-						rs.getString("edesignation"))
-						);
-			}
+			
+				while (cur.hasNext()) {
 
+                    Document doc = cur.next();
+                    list = new ArrayList <>(); 
+
+                    System.out.printf("%s: %s%n", list.get(1), list.get(2),list.get(3));
+                }
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/main/java/com/rntbci/db/DbConn.java
+++ src/main/java/com/rntbci/db/DbConn.java
@@ -3,20 +3,28 @@ package com.rntbci.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import org.bson.Document;
+
+ import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 public class DbConn {
-private static Connection conn=null;
+	public static MongoCollection <Document> collection=null;
 	
-	static {
+	 static {
 		try {
-			Class.forName("mongodb.jdbc.MongoDriver");
-			conn= DriverManager.getConnection("jdbc:mongo://127.0.0.1:27017/radb", "RAG", "NIT");
+		MongoClient mongoClient = new MongoClient("mongodb://127.0.0.1:27017");
+		 MongoDatabase database = mongoClient.getDatabase("radb");
+		 
+		 collection = database.getCollection("RAG");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public static Connection getConn() {
-		return conn;
+	public static MongoCollection<Document> getConn() {
+		return collection;
 	}
 
 }
